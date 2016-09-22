3f0de5e578b9b7b371dac85354a1e435ee9f783e
Pieter <pieter.eendebak@gmail.com>
2016-09-22 18:52:52 +0200

    improve speed of isConferenceFoldover; fix possible false check

--- src/conference.cpp
+++ src/conference.cpp
@@ -265,13 +265,22 @@ bool isConferenceFoldover ( const array_link &al, int verbose )
     array_link alt = al.transposed();
     array_link alt2 = alt*-1;
 
+	std::vector<int> ri(al.n_rows);
+	std::fill(ri.begin(), ri.end(), -1);
+	
     for ( int i=0; i<al.n_rows; i++ ) {
+		if (ri[i]>-1)
+			continue;
         array_link alx = alt.selectColumns ( i );
         int foundcol=0;
-        for ( int j=0; j<al.n_rows; j++ ) {
+        for ( int j=i; j<al.n_rows; j++ ) {
+			if (ri[j]>-1 )
+				continue;
             array_link alx2 = alt2.selectColumns ( j );
             if ( alx==alx2 ) {
                 foundcol=1;
+				ri[i]=j;
+				ri[j]=i;
                 break;
             }
         }
