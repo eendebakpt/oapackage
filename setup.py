f29e664e938db86c29cf54ed5ca47794d6aca43f
Pieter Eendebak <pieter.eendebak@gmail.com>
2019-10-25 22:51:56 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -402,6 +402,7 @@ setup(name='OApackage',
                    'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6',
                    'Programming Language :: Python :: 3.7',
+                   "Programming Language :: Python :: 3.8",
                    'License :: OSI Approved :: BSD License'
                    ]
       )
