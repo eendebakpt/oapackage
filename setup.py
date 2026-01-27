27ee68baeac919efc7f671192544873059994411
Pieter Eendebak <pieter.eendebak@gmail.com>
2026-01-27 23:25:34 +0100

    revert license move

--- setup.py
+++ setup.py
@@ -382,5 +382,6 @@ setup(
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
         "Programming Language :: Python :: 3.14",
+        "License :: OSI Approved :: BSD License",
     ],
 )
